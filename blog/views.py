a90e9f9184469215cb344f6bde83b3cefea2b0ed
vedicvamsi <samvamsi55555@gmail.com>
2021-06-28 14:03:26 +0530

    tweaks fix

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
 def allblog(request):
-    blogs = Blog.objects
+    blogs = Blog.objects.order_by('id').reverse()
     return render(request, 'blog/allblog.html', {'blogs': blogs})
 
 def detail(request, blog_id):
--- jobs/views.py
+++ jobs/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Job
 # Create your views here.
 def home(request):
-    jobs = Job.objects
+    jobs = Job.objects.order_by('id').reverse()
     return render(request,'jobs/home.html',{'jobs':jobs})
